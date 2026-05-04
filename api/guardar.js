a9d2ab4bff88e66d288445557d41e0ebc90b9814
Esteban <edariozzi@hotmail.com.ar>
2026-05-04 12:17:02 -0300

    elimina build viejo

--- api/guardar.js
+++ api/guardar.js
@@ -19,9 +19,4 @@ export default async function handler(req, res) {
 }
 
 
-import { kv } from "@vercel/kv";
 
-export default async function handler(req, res) {
-    await kv.del("turnos");
-    return res.json({ ok: true, message: "Datos borrados" });
-}
